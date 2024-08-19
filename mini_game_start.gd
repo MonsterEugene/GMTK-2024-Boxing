e57dfd030dac14830526732ca6a7b0468b52ade5
Dalton <monstereugene07@gmail.com>
2024-08-18 18:32:53 -0600

    laying foundations

---
+++ mini_game_start.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@export var miniGame = "gym"
+
+var s = false
+func _on_body_entered(body):
+	if body is trainer:
+		#make button thing appear
+		
+		s = true
+		
+
+func _input(event):
+	if Input.is_action_just_pressed('space') && s == true:
+		print("minigame")
+		get_tree().change_scene_to_file("res://" + miniGame + ".tscn")
+		
+
+func _on_body_exited(body):
+	s = false
+	
+	#make button disapear? 
